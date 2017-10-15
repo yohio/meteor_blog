aac23924baa47522333f1e6e5fdfe3008aa66951
Yohai Ararat <a_yohi@hotmail.com>
2017-10-15 13:22:57 -0400

    adding the button for the delete post

--- client/posts/templates/post_page.js
+++ client/posts/templates/post_page.js
@@ -5,3 +5,25 @@ Template.postPage.helpers({
         return currentUser.username == instance.data.author;
     }
 });
+
+Template.postPage.events({
+    'click #deletePosts' (event, instance) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        new Confirmation({
+            message: "Are you sure ?",
+            title: "Delete Post",
+            cancelText: "Cancel",
+            okText: "Ok",
+            success: true,
+            focus: "cancel"
+        }, function (ok) {
+            if(ok) {
+                Router.go('root');
+                Meteor.call('deletePost', instance.data._id);
+            }
+
+        });
+    }
+});
